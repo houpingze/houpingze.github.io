de69930ba0ef68e63433a27ca2435bdf3a0fe77f
houpingze <houpingze@163.com>
2021-08-14 14:53:04 +0800

    1

---
+++ PJ模拟赛.cpp
@@ -0,0 +1,47 @@
+/*houpingze \CCF/ \CCF/ \CCF/ \CCF/ \CCF/ */
+/*
+Note:
+
+*/
+#include<bits/stdc++.h>
+#define reg register int
+#define INF (1<<30)
+#define pb push_back
+#define vc vector
+#define fst first
+#define scd second
+//#define int long long
+#define rep(i,x,y) for(int i=x;i<=y;i++)
+using namespace std;
+int read(){
+	int res=0,fs=1; char c=getchar();
+	while(!(c>='0' && c<='9')){ if(c=='-')fs=-1; c=getchar(); }
+	while(c>='0' && c<='9')res=res*10+c-'0',c=getchar();
+	return res*fs;
+}
+void print(int x){
+    if(x<0) { putchar('-'); x=-x;}
+    if(x>9) print(x/10);
+    putchar(x%10+'0');
+}
+int cnt,a[5010],ans,tmp;
+typedef pair<int,int> P;
+//struct PROBLEM_SOLVER{
+//	   int n,m;
+//}solver;
+
+//signed main(){
+int n,s,k,v;
+int main() {
+	ios::sync_with_stdio(false);
+	cin>>tmp;
+	while(tmp--){
+		cin>>n>>s>>k>>v;
+		if(s==0) cout<<0<<endl;
+		else 
+		cout<<-2147483648<<endl;
+	}
+    return 0;
+}
+
+
